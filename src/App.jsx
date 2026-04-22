aeadb5073644952675278811900b5389571e01a6
Adriel <43111@raphaeldisanto.com.br>
2026-04-21 21:24:34 -0300

    atualizando App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-
+import neymar from "./assets/neymar.jpg";
 function App() {
   const frases = [
-    "O segredo é nunca desistir ⚽",
-    "Treine enquanto eles dormem 💪",
-    "O impossível é só questão de opinião 🔥",
-    "Jogue com alegria 😄",
-    "Cada dia é uma nova chance 🚀",
-    "Foco, força e fé 🙏",
-    "Se divirta jogando bola ⚽",
-    "Sonhe grande e trabalhe duro 💯"
+    "O segredo é nunca desistir ",
+    "Treine enquanto eles dormem ",
+    "O impossível é só questão de opinião ",
+    "Jogue com alegria ",
+    "Cada dia é uma nova chance ",
+    "Foco, força e fé ",
+    "Se divirta jogando bola ",
+    "Sonhe grande e trabalhe duro "
   ];
 
   const [frase, setFrase] = useState("");
@@ -23,7 +23,7 @@ function App() {
     <div className="container">
       <h1>Frases do neymar</h1>
       <p>Motivação de craque</p>
-
+      <img src={neymar} alt="Neymar" className="neymar-img" />
       <button onClick={gerarFrase}>Gerar Frase</button>
 
       {frase && (
